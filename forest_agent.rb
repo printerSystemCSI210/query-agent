95a0cb4a51396a88c27ef05c51d982a2cb480e15
Ben Burwell <bburwell1@gmail.com>
2013-11-25 11:34:56 -0500

    Minor fixes

--- forest_agent.rb
+++ forest_agent.rb
@@ -5,8 +5,8 @@ require 'forest-client'
 def setup
     
     # get ID from user
-    puts "\n        Welcome to Forest! \n"
-    puts "---------------------------------- \n"
+    puts "\n                 Welcome to Forest! \n"
+    puts "----------------------------------------------------- \n"
     puts "Please enter your 24-character Organization ID below."
     print "id: "
     id = gets.chomp
@@ -15,6 +15,8 @@ def setup
     file = File.open('forest.id', 'w')
     file.write(id)
     file.close
+
+    puts "Thanks! You won't have to do that again :)"
     
     return id
 end
@@ -33,15 +35,20 @@ def read_config
 end
 
 def main
+    
     begin
+        # Try to read ID
         id = read_config
     rescue
+        # If we can't read the ID, prompt the user
         id = setup
     end
 
-
     qa = ForestClient::QueryAgent.new(id)
     qa.run
+
+    puts "We just queried the printers you've added to Forest!"
+    puts "Check your web console for details!"
 end
 
 main
